182982c829e97a2cf0b5f326c34ae9303a3be7a0
lomadi <lomadi@mac.com>
2020-06-01 02:01:50 +0200

    add events images

---
+++ images/taiga-events/start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sed -e 's/$RABBIT_USER/'$RABBIT_USER'/' \
+	-e 's/$RABBIT_PASSWORD/'$RABBIT_PASSWORD'/' \
+	-e 's/$RABBIT_HOST/'$RABBIT_HOST'/' \
+	-e 's/$RABBIT_PORT/'$RABBIT_PORT'/' \
+	-e 's/$RABBIT_VHOST/'$RABBIT_VHOST'/' \
+	-e 's/$TAIGA_SECRET/'$TAIGA_SECRET'/' \
+	-i config.json
+
+exec coffee index.coffee > /dev/stdout 2> /dev/stderr &
+NODE_PID=$!
+
+trap 'kill -TERM $NODE_PID' SIGTERM
+
+wait $NODE_PID
