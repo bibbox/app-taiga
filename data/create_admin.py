6c390541b385d975512fdbf8b47a06a8aaf8d25e
Emilian <emilian.jung+github@gmail.com>
2022-03-01 16:03:01 +0100

    changed default pw

--- data/create_admin.py
+++ data/create_admin.py
@@ -3,7 +3,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settings.common")
 from django.core.management.base import BaseCommand, CommandError
 from django.contrib.auth import get_user_model
 User=get_user_model()
-user=User.objects.create_user(os.getenv('ADMIN_USER', 'admin'), email=os.getenv('ADMIN_EMAIL', 'admin@bibbox.org'), password=os.getenv('ADMIN_PASSWORD', 'psw'))
+user=User.objects.create_user(os.getenv('ADMIN_USER', 'admin'), email=os.getenv('ADMIN_EMAIL', 'admin@bibbox.org'), password=os.getenv('ADMIN_PASSWORD', 'vendetta'))
 user.is_superuser=True
 user.is_staff=True
 user.save()
